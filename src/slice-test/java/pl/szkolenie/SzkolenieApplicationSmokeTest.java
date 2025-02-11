de7e21ad436bff0a65d940b25b63d3fad4ac1bed
mgrzesiak <dsa@das.pl>
2025-02-11 21:35:41 +0100

    move smoke test to slice-test

--- src/slice-test/java/pl/szkolenie/SzkolenieApplicationSmokeTest.java
+++ src/slice-test/java/pl/szkolenie/SzkolenieApplicationSmokeTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SzkolenieApplicationTest {
+class SzkolenieApplicationSmokeTest {
 
 	@Test
 	void contextLoads() {
