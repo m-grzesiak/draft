2779d83e0594a6be3ea3187da0d1e2932f4e9a3e
mgrzesiak <dsa@das.pl>
2025-02-11 21:35:12 +0100

    move smoke test to slice-test

--- src/slice-test/java/pl/szkolenie/SzkolenieApplicationSmokeTest.java
+++ src/slice-test/java/pl/szkolenie/SzkolenieApplicationSmokeTest.java
@@ -2,11 +2,9 @@ package pl.szkolenie;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
-class SzkolenieApplicationTests {
+class SzkolenieApplicationTest {
 
 	@Test
 	void contextLoads() {
--- src/test/java/pl/szkolenie/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package pl.szkolenie;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
